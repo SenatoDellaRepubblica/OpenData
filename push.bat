f4dc3cf7b56cd56697981996745a594d924ca0f7
giovanni.lalle@senato.it <giovanni.lalle@senato.it>
2023-11-28 15:29:32 +0100

    2023-11-03

--- push.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-
-SET LEG=19
-SET REMOTE_DIR=\\senato.intranet\Utenti\Homes\lalle\dump-opendata
-SET WORKING_DIR=%~dp0
-echo %WORKING_DIR%
-
-copy %REMOTE_DIR%\*.zip %WORKING_DIR%\Leg%LEG%
